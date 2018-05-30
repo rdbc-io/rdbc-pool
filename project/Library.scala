fb065b144824f64223c3a2f9fff5c256eb3af988
Krzysztof Pado <povder@gmail.com>
2018-05-29 21:15:11 -0700

    Update dependencies

--- project/Library.scala
+++ project/Library.scala
@@ -1,16 +1,16 @@
 import sbt._
 
 object Library {
-  private val rdbcVersion = "0.0.77"
-  private val unipoolVersion = "0.0.3"
+  private val rdbcVersion = "0.0.80"
+  private val unipoolVersion = "0.0.4"
 
   val unipoolScala = "io.github.povder.unipool" %% "unipool-scala" % unipoolVersion
   val unipoolJava = "io.github.povder.unipool" %% "unipool-java" % unipoolVersion
   val rdbcScalaApi = "io.rdbc" %% "rdbc-api-scala" % rdbcVersion
-  val rdbcJavaApi = "io.rdbc" %% "rdbc-api-java" % rdbcVersion
+  val rdbcJavaApi = "io.rdbc" % "rdbc-api-java" % rdbcVersion
   val rdbcImplbase = "io.rdbc" %% "rdbc-implbase" % rdbcVersion
   val rdbcJavaAdapter = "io.rdbc" %% "rdbc-java-adapter" % rdbcVersion
-  val immutables = "org.immutables" % "value" % "2.5.6"
+  val immutables = "org.immutables" % "value" % "2.6.1"
   val slf4j = "org.slf4j" % "slf4j-api" % "1.7.25"
 
   val scalatest = "org.scalatest" %% "scalatest" % "3.0.5"
